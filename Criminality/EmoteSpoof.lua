76562fbaabe7d9e0fce75879fe3da765907bcd8d
Pigeon <pigeon5544@tutanota.com>
2024-05-03 18:39:51 +0100

    Update EmoteSpoof.lua

--- Criminality/EmoteSpoof.lua
+++ Criminality/EmoteSpoof.lua
@@ -1,3 +1,5 @@
+-- Doesn't need anti cheat bypass, after executing u must reset ur character otherwise it wont affect anything
+
 for Index, Data in next, getgc(true) do
     if type(Data) == "table" and rawget(Data, "XIIX") then
         local old; old = hookfunction(getproto(Data.XIIX, 29, true)[1], function(...)
